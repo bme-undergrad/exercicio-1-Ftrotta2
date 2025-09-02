750742369d3c95bc936a6ae9aad780218f2b5886
Ftrotta2 <102884739+Ftrotta2@users.noreply.github.com>
2025-09-02 12:00:49 -0300

    Update 1 exercicio1.m
    teste 1

--- exercicio1.m
+++ exercicio1.m
@@ -7,8 +7,22 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+  t = 0;
+  x = zeros(imax,1);
+  x(1) = x0;
+
+  erros = zeros(imax,1);
+  for j = 1:imax -1
+    x(j+1) = x(j) - (func(x(j)) / func_d(x(j)));
+    t = x(j+1);
+    if j ~=1
+      erros(j) = abs((x(j) - x(j-1)) / x(j));
+      if erros(j) <= es
+        break
+      endif
+    endif
+  endfor
+  t
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
