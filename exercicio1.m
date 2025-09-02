e7e269f491a1c51acbb6a9952695aebd64a1f6f3
Ftrotta2 <102884739+Ftrotta2@users.noreply.github.com>
2025-09-02 12:30:15 -0300

    Update 3 exercicio1.m
    teste 3

--- exercicio1.m
+++ exercicio1.m
@@ -6,24 +6,21 @@ imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  t = 0;
-  x = zeros(imax,1);
-  x(1) = x0;
-
-  erros = zeros(imax,1);
-  for j = 1:imax -1
-    x(j+1) = x(j) - (func(x(j)) / func_d(x(j)));
-    t = x(j+1);
+t = 0;
+x = zeros(imax,1);
+x(1) = x0;
+erros = zeros(imax,1);
+for j = 1:imax -1
+  x(j+1) = x(j) - (func(x(j)) / func_d(x(j)));
+  t = x(j+1);
     if j ~=1
       erros(j) = abs((x(j) - x(j-1)) / x(j));
       if erros(j) <= es
         break
       endif
     endif
-  endfor
-  t
-
+endfor
+t
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
